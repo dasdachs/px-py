c9aad41428bdad3c048e7be0a7b158acf61c116f
Jani Šumak <jani.sumak@inova.si>
2020-11-02 07:10:35 +0100

    Deprecated parser and added ply parser

--- px/px.py
+++ px/px.py
@@ -2,58 +2,62 @@ import json
 from pathlib import Path
 from typing import List, Dict, Optional
 
-import pcaxis
-
+from .parser import parsePX
 from .exceptions import FileNotFoundError
+
+
+__all__ = ["PxPy"]
 
 
 class PxPy:
     """
-    Main class for dealing with px files. It is a collection of parsed px files 
+    Main class for dealing with px files. It is a collection of parsed px files
     for selected languages.
     """
-    def __init__(self, parsed_data: Dict, selected_language: Optional[str]) -> None:
+
+    def __init__(
+        self, parsed_data: Dict, selected_language: Optional[str] = None
+    ) -> None:
         self._data = parsed_data
         self._px_array: List[Px] = []
         self._selected_px_file_index = 0
-
-        for index, file in enumerate(self._data.values()):
-            self._px_array.append(Px(file))
-            if file["LANGUAGE"] == selected_language:
-                self._selected_px_file_index = index
+        print(parsed_data)
+        # for index, file in enumerate(self._data.values()):
+        #     self._px_array.append(Px(file))
+        #     if file["LANGUAGE"] == selected_language:
+        #         self._selected_px_file_index = index
 
     @staticmethod
-    def read_file(path: str, selected_language: Optional[str]) -> PxPy:
+    def read_file(path: str, selected_language: Optional[str] = None):
         p = Path(path)
 
         if not p or not p.exists() or p.is_dir():
             raise FileNotFoundError
-        
-        parsed_file = pcaxis.parsePX(p.open().read(), encoding='utf-8')
 
-        return PxPy(parsed_file, selected_language)
+        try:
+            content = p.read_bytes()
+            parsed_file = parsePX(content, "utf-8")
 
-    
+            return PxPy(parsed_file, selected_language)
+        except UnicodeDecodeError as e:
+            print(f"Opening file throwed an UnicodeDecodeError. Error: {e}")
+
     @staticmethod
     def read_csv() -> None:
         pass
-    
-    
+
     @staticmethod
     def read_excel() -> None:
         pass
-   
-    
+
     @staticmethod
     def read_json() -> None:
         pass
-    
-    
+
     @staticmethod
     def read_dataFrame() -> None:
         pass
-    
-    
+
     @property
     def language(self) -> str:
         return self._px_array[self._selected_px_file_index].language
@@ -62,14 +66,11 @@ class PxPy:
     def title(self) -> str:
         return self._px_array[self._selected_px_file_index].title
 
-
     def to_excel(self) -> None:
         pass
 
-
     def to_csv(self) -> None:
         pass
-
 
     def to_json(self) -> None:
         pass
@@ -81,6 +82,6 @@ class PxPy:
 class Px:
     def __init__(self, data: Dict) -> None:
         self._data = data
-        
+
         self.title = data.get("TITLE")
         self.language = data.get("LANGUAGE")
